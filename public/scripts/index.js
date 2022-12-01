f0c7cdeacc31212e3cf87d5e1d6a80b852c17a32
Mahmouud74 <mahmouudmoohamed@gmail.com>
2022-12-01 16:53:43 +0200

    done

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,7 +4,23 @@ var loginForm= document.createElement("form");
 var userNameRegistration = document.createElement("input");
 var gender
 var emailLogin = document.createElement("input");
+var nameRegex = /[a-zA-Z]{1,}/;
+var emailRegex = /^[a-zA-z]+@+[a-zA-z]+\.+[a-zA-z]{3}$/;
+var mobileRegex = /^(011|012|015|010)[0-9]{8}$/;
+var passwordRegex = /([a-zA-z0-9@]){8,}/;
+var userNameInput;
+var emailInput;
+var phoneInput;
+var passwordInput;
+var emailLoginInput;
+var passwordLoginInput;
 function index(){
+    userNameInput=document.getElementById("userName");
+    emailInput=document.getElementById("email");
+    phoneInput=document.getElementById("phone");
+    passwordInput=document.getElementById("password");
+    emailLoginInput =document.getElementById("emailLogin");
+    passwordLoginInput=document.getElementById("passwordLogin");
     var playerImg=document.getElementById("playerImg");
     let imgPosition = (getComputedStyle(playerImg).left);
     let imgLocation = parseInt(imgPosition)
@@ -24,139 +40,122 @@ function index(){
             
         },500)
     },200)
-
-    /*  var displayPlayerImg = setInterval(function(){
-     if(imgLocation==750){
-        document.getElementById("introDesc").style="display:inline-block"
-        clearInterval=displayPlayerImg
-     }
-     else{
-        imgLocation+=250
-        playerImg.style.left=imgLocation+"px";
-     }
-        
-    },200)*/
     var labels = document.getElementsByTagName('label');
-    userNameRegistration.type="text";
-    userNameRegistration.placeholder="enter your name";
-    registerationForm.appendChild(userNameRegistration);
-    console.log(registerationForm);
-    //img = document.getElementById("img");
-    //console.log(img);
+
     document.getElementById("registrationLink").addEventListener("click",function(){
-        /*console.log(document.getElementsByClassName("formContainer")[0]);
-        if(document.getElementsByClassName("formContainer")[0].firstElementChild==loginForm){
-            document.getElementsByClassName("formContainer")[0].removeChild(loginForm)
-        }
-        else{
-            document.getElementsByClassName("formContainer")[0].innerHTML="";
-        }
-        document.getElementsByClassName("formContainer")[0].appendChild(registerationForm);*/
+    
         document.getElementById("loginForm").style="display:none;";
         document.getElementById("registerationForm").style="display:inline-block;";
         document.getElementById("login").style.borderBottom="none";
         document.getElementById("register").style.borderBottom="2px solid rgb(251, 57, 73)";
     })
     document.getElementById("logInLink").addEventListener("click",function(){
-       /* emailLogin.type="email";
-        emailLogin.placeholder="enter your mail";
-        loginForm.appendChild(emailLogin);
-        console.log(document.getElementsByClassName("formContainer")[0]);
-        if(document.getElementsByClassName("formContainer")[0].firstElementChild==registerationForm){
-            document.getElementsByClassName("formContainer")[0].removeChild(registerationForm)
-        }
-        else{
-            document.getElementsByClassName("formContainer")[0].innerHTML="";
-        }
-        document.getElementsByClassName("formContainer")[0].innerHTML="";
-        document.getElementsByClassName("formContainer")[0].appendChild(loginForm)*/
+
         document.getElementById("registerationForm").style="display:none;";
         document.getElementById("loginForm").style="display:inline-block;";
         document.getElementById("register").style.borderBottom="none";
         document.getElementById("login").style.borderBottom="2px solid rgb(251, 57, 73)";
     })
-    document.getElementById("userName").addEventListener("focus",function(){
+    console.log(userNameInput);
+    userNameInput.addEventListener("focus",function(){
         for (let i = 0; i < labels.length; i++) {
             if (labels[i].htmlFor == 'userName') {
               labels[i].style="visibility:visible";
            }
         }
     })
-    document.getElementById("userName").addEventListener("blur",function(){
-        for (let i = 0; i < labels.length; i++) {
+    userNameInput.addEventListener("blur",function(){
+        if(document.getElementById("userNameCheckMark").style.display!="inline"){
+                    for (let i = 0; i < labels.length; i++) {
             if (labels[i].htmlFor == 'userName') {
               labels[i].style="visibility:hidden";
            }
         }
+        }
+
     })
-    document.getElementById("email").addEventListener("focus",function(){
+    emailInput.addEventListener("focus",function(){
         for (let i = 0; i < labels.length; i++) {
             if (labels[i].htmlFor == 'email') {
               labels[i].style="visibility:visible";
            }
         }
     })
-    document.getElementById("email").addEventListener("blur",function(){
-        for (let i = 0; i < labels.length; i++) {
-            if (labels[i].htmlFor == 'email') {
-              labels[i].style="visibility:hidden";
-           }
+    emailInput.addEventListener("blur",function(){
+        if(document.getElementById("emailCheckMark").style.display!="inline"){
+            for (let i = 0; i < labels.length; i++) {
+                if (labels[i].htmlFor == 'email') {
+                  labels[i].style="visibility:hidden";
+               }
+            }
         }
+
     })
-    document.getElementById("phone").addEventListener("focus",function(){
+    phoneInput.addEventListener("focus",function(){
         for (let i = 0; i < labels.length; i++) {
             if (labels[i].htmlFor == 'phone') {
               labels[i].style="visibility:visible";
            }
         }
     })
-    document.getElementById("phone").addEventListener("blur",function(){
-        for (let i = 0; i < labels.length; i++) {
-            if (labels[i].htmlFor == 'phone') {
-              labels[i].style="visibility:hidden";
-           }
+    phoneInput.addEventListener("blur",function(){
+        if(document.getElementById("phoneCheckMark").style.display!="inline"){
+            for (let i = 0; i < labels.length; i++) {
+                if (labels[i].htmlFor == 'phone') {
+                  labels[i].style="visibility:hidden";
+               }
+            }
         }
+
     })
-    document.getElementById("password").addEventListener("focus",function(){
+    passwordInput.addEventListener("focus",function(){
         for (let i = 0; i < labels.length; i++) {
             if (labels[i].htmlFor == 'password') {
               labels[i].style="visibility:visible";
            }
         }
     })
-    document.getElementById("password").addEventListener("blur",function(){
-        for (let i = 0; i < labels.length; i++) {
-            if (labels[i].htmlFor == 'password') {
-              labels[i].style="visibility:hidden";
-           }
+    passwordInput.addEventListener("blur",function(){
+        if(document.getElementById("passwordCheckMark").style.display!="inline"){
+            for (let i = 0; i < labels.length; i++) {
+                if (labels[i].htmlFor == 'password') {
+                  labels[i].style="visibility:hidden";
+               }
+            }
         }
+
     })
-    document.getElementById("emailLogin").addEventListener("focus",function(){
+    emailLoginInput.addEventListener("focus",function(){
         for (let i = 0; i < labels.length; i++) {
             if (labels[i].htmlFor == 'emailLogin') {
               labels[i].style="visibility:visible";
            }
         }
     })
-    document.getElementById("emailLogin").addEventListener("blur",function(){
-        for (let i = 0; i < labels.length; i++) {
-            if (labels[i].htmlFor == 'emailLogin') {
-              labels[i].style="visibility:hidden";
-           }
+    emailLoginInput.addEventListener("blur",function(){
+        if(document.getElementById("emailLoginCheckMark").style.display!="inline"){
+            for (let i = 0; i < labels.length; i++) {
+                if (labels[i].htmlFor == 'emailLogin') {
+                  labels[i].style="visibility:hidden";
+               }
+            }
         }
+
     })
-    document.getElementById("passwordLogin").addEventListener("focus",function(){
+    passwordLoginInput.addEventListener("focus",function(){
         for (let i = 0; i < labels.length; i++) {
             if (labels[i].htmlFor == 'passwordLogin') {
               labels[i].style="visibility:visible";
            }
         }
     })
-    document.getElementById("passwordLogin").addEventListener("blur",function(){
-        for (let i = 0; i < labels.length; i++) {
-            if (labels[i].htmlFor == 'passwordLogin') {
-              labels[i].style="visibility:hidden";
-           }
+    passwordLoginInput.addEventListener("blur",function(){
+        if(document.getElementById("passwordLoginCheckMark").style.display!="inline"){    
+            for (let i = 0; i < labels.length; i++) {
+                if (labels[i].htmlFor == 'passwordLogin') {
+                labels[i].style="visibility:hidden";
+            }
+            }
         }
     })
     document.getElementById("register").addEventListener("click",function(){
@@ -172,5 +171,67 @@ function index(){
         document.getElementById("register").style.borderBottom="none";
         this.style.borderBottom="2px solid rgb(251, 57, 73)";
         event.preventDefault();
+    })
+    userNameInput.addEventListener('keyup',function(){
+        if(nameRegex.test(userNameInput.value)){
+            document.getElementById("userNameWarning").style.display="none";
+            document.getElementById("userNameCheckMark").style.display="inline";
+                    }
+        else{
+            document.getElementById("userNameCheckMark").style.display="none";
+            document.getElementById("userNameWarning").style.display="block";
+        }
+    })
+    emailInput.addEventListener('keyup',function(){
+        if(emailRegex.test(emailInput.value)){
+            document.getElementById("emailWarning").style.display="none";
+            document.getElementById("emailCheckMark").style.display="inline";
+                 }
+        else{
+            document.getElementById("emailCheckMark").style.display="none";
+            document.getElementById("emailWarning").style.display="block";
+        }
+    })
+    phoneInput.addEventListener('keyup',function(){
+        if(mobileRegex.test(phoneInput.value)){
+            document.getElementById("phoneWarning").style.display="none";
+            document.getElementById("phoneCheckMark").style.display="inline";
+                 }
+        else{
+            document.getElementById("phoneCheckMark").style.display="none";
+            document.getElementById("phoneWarning").style.display="block";
+        }
+    })
+    passwordInput.addEventListener('keyup',function(){
+        if(passwordRegex.test(passwordInput.value)){
+            document.getElementById("passwordWarning").style.display="none";
+            document.getElementById("passwordCheckMark").style.display="inline";
+                    }
+        else{
+            document.getElementById("passwordCheckMark").style.display="none"
+            document.getElementById("passwordWarning").style.display="block";
+        }
+    })
+    emailLoginInput.addEventListener('keyup',function(){
+        if(emailRegex.test(emailLoginInput.value)){
+            document.getElementById("emailLoginWarning").style.display="none";
+            document.getElementById("emailLoginCheckMark").style.display="inline";
+
+            
+        }
+        else{
+            document.getElementById("emailLoginCheckMark").style.display="none";
+            document.getElementById("emailLoginWarning").style.display="block";
+        }
+    })
+    passwordLoginInput.addEventListener('keyup',function(){
+        if((passwordLoginInput.value)){
+            document.getElementById("passwordLoginCheckMark").style.display="inline";
+
+            
+        }
+        else{
+            document.getElementById("passwordLoginCheckMark").style.display="none";
+        }
     })
 }
